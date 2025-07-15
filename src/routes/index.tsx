f6ed0a6feefa9cb663fea2088bb9d7daeb4cce36
wookhyung <qpflapffhs76@gmail.com>
2025-07-15 22:08:50 +0900

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { HomePage } from '~/pages/home/ui/home-page';
+
+export const Route = createFileRoute('/')({
+  component: HomePage,
+});
